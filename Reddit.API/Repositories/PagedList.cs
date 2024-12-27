591aa5be42d6acb0f1c2bd203d24c753485b3313
anri <kezeroti2@gmail.com>
2024-12-27 15:29:21 +0400

    Add Repository tests

--- Reddit.API/Repositories/PagedList.cs
+++ Reddit.API/Repositories/PagedList.cs
@@ -21,14 +21,25 @@ namespace Reddit.Repositories
         public bool HasNextPage { get; set; }
         public bool HasPreviousPage { get; set; }
 
-        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> items, int page, int pageSize)
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> items, int pageNumber, int pageSize)
         {
-            var pagedItems = await items.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
-            var totalCount = await items.CountAsync();
-            var hasNextPage = (page * pageSize) < totalCount;
-            var hasPreviousPage = page > 1;
+            Validate(pageNumber, pageSize);
 
-            return new PagedList<T>(pagedItems, page, pageSize, totalCount, hasNextPage, hasPreviousPage);
+            var pagedItems = await items.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            var totalCount = await items.CountAsync();
+            var hasNextPage = (pageNumber * pageSize) < totalCount;
+            var hasPreviousPage = pageNumber > 1;
+
+            return new PagedList<T>(pagedItems, pageNumber, pageSize, totalCount, hasNextPage, hasPreviousPage);
+        }
+
+        private static void Validate(int pageNumber, int pageSize)
+        {
+            if (pageNumber <= 0)
+                throw new ArgumentOutOfRangeException(nameof(pageNumber),"pageNumber and pageSize must be greater than 0");
+
+            if(pageSize <= 0 || pageSize > 50) 
+                throw new ArgumentOutOfRangeException(nameof(pageSize),"pageSize must be greater than 0 and less than 50");
         }
     }
 }
