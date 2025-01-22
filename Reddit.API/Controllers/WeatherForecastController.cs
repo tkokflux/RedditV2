123388d26c5b4600c397fb26959c867325ff90c4
anri <kezeroti2@gmail.com>
2025-01-22 15:58:04 +0400

    Weather Forcast Logging

--- Reddit.API/Controllers/WeatherForecastController.cs
+++ Reddit.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Reddit.Controllers
 {
@@ -21,13 +25,37 @@ namespace Reddit.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // Log the start of the operation
+            _logger.LogInformation("WeatherForecastController.Get - Starting to generate weather forecasts.");
+
+            try
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+                // Log the successful generation of forecasts
+                _logger.LogInformation("WeatherForecastController.Get - Successfully generated {Count} weather forecasts.", forecasts.Length);
+
+                return forecasts;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception with details
+                _logger.LogError(ex, "WeatherForecastController.Get - An error occurred while generating weather forecasts.");
+
+                // Re-throw the exception to be handled by the global exception handler
+                throw;
+            }
+            finally
+            {
+                // Log the end of the operation
+                _logger.LogInformation("WeatherForecastController.Get - Completed generating weather forecasts.");
+            }
         }
     }
 }
